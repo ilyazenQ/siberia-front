4915d7349ce61b281e4fc06ab87728140d0fb953
Ilya <ilyazenq@gmail.com>
2021-11-09 19:33:44 +0800

    ref

--- #source/js/map.js
+++ #source/js/map.js
@@ -1,6 +1,31 @@
-let test = document.getElementById('test');
+//let test = document.getElementById('test');
 
-test.addEventListener('click',(e) => {  
-  document.querySelector('.accordion-button').click();
-  document.querySelector('.accordion-button').focus();
+//test.addEventListener('click',(e) => {  
+//  document.querySelector('.accordion-button').click();
+//  document.querySelector('.accordion-button').focus();
+//});
+
+const compas = document.querySelector('.compas-svg'),
+compasKamchatka = document.querySelector('.kamchatka'),
+compasMongolia = document.querySelector('.mongolia'),
+compasArrow = document.querySelector('.compas-arrow');
+
+compasMongolia.addEventListener('mouseenter', function(e){
+  
+  compasArrow.classList.add('change-mongolia');
+  compasArrow.classList.remove('change-kamchatka');
+ 
 });
+
+
+compasKamchatka.addEventListener('mouseenter', function(e){
+  
+  compasArrow.classList.add('change-kamchatka');
+  compasArrow.classList.remove('change-mongolia');
+  
+});
+
+
+     // transform-box: fill-box;
+     // transform-origin:15% 79%;
+      //transform:rotate(90deg);
