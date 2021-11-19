eadca92d14492b708786943a209d9dfc823badb2
Ilya <ilyazenq@gmail.com>
2021-11-19 18:38:03 +0800

    new calendar

---
+++ #source/js/calendar.js
@@ -0,0 +1,12 @@
+new ScrollBooster({
+   viewport: document.querySelector('.calendar-body'),
+   content: document.querySelector('.calendar-body'),
+   scrollMode: 'native',
+   direction: 'horizontal'
+ });
+ let rightMover = document.querySelector('.calendar-move-right');
+ let calendarBody = document.querySelector('.calendar-body');
+ function scrollRight() {
+   calendarBody.scrollLeft += 150;
+}
+ rightMover.addEventListener('click',scrollRight)
