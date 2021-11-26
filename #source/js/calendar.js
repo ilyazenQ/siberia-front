deef80cd161c5aef67e190cc0d205d8c2a19d431
Ilya <ilyazenq@gmail.com>
2021-11-26 12:23:42 +0800

    m

--- #source/js/calendar.js
+++ #source/js/calendar.js
@@ -1,9 +1,12 @@
-new ScrollBooster({
-   viewport: document.querySelector('.calendar-body'),
-   content: document.querySelector('.calendar-body'),
-   scrollMode: 'native',
-   direction: 'horizontal'
+if(window.screen.width > 1024)
+{
+  new ScrollBooster({
+  viewport: document.querySelector('.calendar-body'),
+  content: document.querySelector('.calendar-body'),
+ scrollMode: 'native',
+  direction: 'horizontal'
  });
+}
  let rightMover = document.querySelector('.calendar-move-right');
  let calendarBody = document.querySelector('.calendar-body');
  function scrollRight() {
