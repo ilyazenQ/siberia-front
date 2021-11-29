c40e054c62612dd7262f42469196128cbae25aa7
Ilya <ilyazenq@gmail.com>
2021-11-29 15:15:31 +0800

    -

--- #source/js/single-day.js
+++ #source/js/single-day.js
@@ -30,3 +30,38 @@ function openClose(el) {
 days.forEach(el => {
   el.addEventListener('click',openClose)
 });
+
+if(window.screen.width > 1024)
+{
+  new ScrollBooster({
+  viewport: document.querySelector('.section-see'),
+  content: document.querySelector('.section-see'),
+ scrollMode: 'native',
+  direction: 'horizontal'
+ });
+}
+ let rightMoverArr = document.querySelectorAll('.calendar-move-right');
+ let calendarBody = document.querySelector('.section-see');
+ function scrollRight() {
+   calendarBody.scrollLeft += 150;
+   rightMoverArr.forEach(el => {
+   let currentRight = parseInt(el.style.right);
+   el.style.right = currentRight - 150 + 'px';
+    
+   
+  });
+}
+rightMoverArr.forEach(el => {
+  el.addEventListener('click',scrollRight)
+ 
+});
+calendarBody.addEventListener('scroll',scrollBody)
+function scrollBody() {
+//	console.log(calendarBody.scrollLeft);
+ rightMoverArr.forEach(el => {
+  let currentRight = parseInt(el.style.right);
+
+  el.style.right += currentRight - 
+  parseInt(calendarBody.scrollLeft,10) + 'px';
+});
+}
