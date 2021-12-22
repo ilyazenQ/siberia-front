f32ebd7275ba6522259b9ab47558d78ab72f11bb
Ilya <ilyazenq@gmail.com>
2021-12-22 13:26:14 +0800

    +

---
+++ #source/js/animate-on-scroll.js
@@ -0,0 +1,34 @@
+let animItems = document.querySelectorAll('.anim-items');
+if(animItems.length > 0 && window.screen.width < 800) {
+   window.addEventListener('scroll',animOnScroll);
+   function animOnScroll(param) {
+      for (let index = 0; index < animItems.length; index++) {
+         const animItem = animItems[index];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 1;
+
+         let animItemPiont = window.innerHeight - animItemHeight / animStart;
+
+         if(animItemHeight > window.innerHeight) {
+            let animItemPiont = window.innerHeight - window.innerHeight / animStart;
+         }
+
+         if((pageYOffset > animItemOffset - animItemPiont) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add("_animate");
+         } else {
+            animItem.classList.remove("_animate");
+         }
+      }
+      function offset(el) {
+         const rect = el.getBoundingClientRect(),
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+         return {top:rect.top + scrollTop, left:rect.left + screenLeft}
+      }
+
+      setTimeout(() => {
+         animOnScroll()
+      },1000);
+   }
+}
