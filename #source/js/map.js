13529780d757f334cbeb66ef319cbf416ecefa80
Ilya <ilyazenq@gmail.com>
2021-12-28 18:03:57 +0800

    add overflow to list on main

--- #source/js/map.js
+++ #source/js/map.js
@@ -5,25 +5,41 @@
 //  document.querySelector('.accordion-button').focus();
 //});
 
+const accordionInMap = document.getElementById("accordionExample");
+accordionInMap.addEventListener('click', function(e){
+  if(window.screen.width <= 511) {
+  accordionInMap.style.paddingBottom = "80px";
+  }
+}
+);
+
 const compas = document.querySelector('.compas-svg'),
 compasKamchatka = document.querySelector('.kamchatka'),
 compasMongolia = document.querySelector('.mongolia'),
 compasArrow = document.querySelector('.compas-arrow');
+let deg = 0;
+function moveTheArrow(deg) {
+  compasArrow.style.transform = `rotate(${deg}deg)`;
+}
+setInterval(() => {
+  moveTheArrow(deg);
+  deg++;
+},40)
 
-compasMongolia.addEventListener('mouseenter', function(e){
+//compasMongolia.addEventListener('mouseenter', function(e){
   
-  compasArrow.classList.add('change-mongolia');
-  compasArrow.classList.remove('change-kamchatka');
+ // compasArrow.classList.add('change-mongolia');
+//  compasArrow.classList.remove('change-kamchatka');
  
-});
+//});
 
 
-compasKamchatka.addEventListener('mouseenter', function(e){
+//compasKamchatka.addEventListener('mouseenter', function(e){
   
-  compasArrow.classList.add('change-kamchatka');
-  compasArrow.classList.remove('change-mongolia');
+//  compasArrow.classList.add('change-kamchatka');
+//  compasArrow.classList.remove('change-mongolia');
   
-});
+//});
 
 
      // transform-box: fill-box;
